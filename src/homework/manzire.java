8c272e9969c36df5b5dc1aad76d9ce7695b19070
reboot_qa <antsshoes6@gmail.com>
2024-02-08 20:54:06 -0500

    replied to manzire

--- src/homework/manzire.java
+++ src/homework/manzire.java
@@ -4,5 +4,9 @@ public class manzire {
 public static void main(String[] args) {
 	System.out.println("hello my name is manzire");
 	System.out.println("how are you today?");
+	
+	
+	System.out.println("Im good manzire - anthony");
+	System.out.println("How are you today?");
 }
 }
