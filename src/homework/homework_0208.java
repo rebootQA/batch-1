3c26b490b3a3a9b3542e6b500af0662a3ff9a626
reboot_qa <antsshoes6@gmail.com>
2024-02-12 18:55:47 -0500

    finished hw

--- src/homework/homework_0208.java
+++ src/homework/homework_0208.java
@@ -1,9 +1,22 @@
 package homework;
 
 public class homework_0208 {
-	
-	//Write me a loop that will print this array in reverse
-	//	{t, o, o, b, e, r}
-	//check if the array has a letter "r" in it.
+	public static void main(String[] args) {
+
+		String[] reboot = { "t", "o", "o", "b", "e", "r" };
+
+		for (String letters : reboot) {
+			if (letters.contains("r")) {
+				for (int i = reboot.length - 1; i >= 0; i--) {
+					System.out.print(reboot[i]);
+				}
+			}
+		}
+
+	}
 
 }
+
+// Write me a loop that will print this array in reverse
+// {t, o, o, b, e, r}
+// check if the array has a letter "r" in it.
