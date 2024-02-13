352163824f2dde530e911a99444bbbe4e2550188
manzire <manzire.anser@gmail.com>
2024-02-13 18:17:10 -0500

    hh

--- src/homework/homework_0212.java
+++ src/homework/homework_0212.java
@@ -3,8 +3,9 @@ package homework;
 public class homework_0212 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		// TODO Auto-generated method stub name = new ();
+	System.out.println("hh");
 	}
 
 }
