e0d2733d2df7cdb610b8ff080f029d947baae218
reboot_qa <antsshoes6@gmail.com>
2024-02-12 20:59:14 -0500

    sadlkads

--- src/practice_here/conflict.java
+++ src/practice_here/conflict.java
@@ -3,10 +3,16 @@ package practice_here;
 public class conflict {
 
 	public static void main(String[] args) {
+		
+		
+		
 		System.out.println("Hello there");
-		System.out.println("Lets have a conflict");
-		System.out.println("Are you having a good day?");
-		System.out.println("Thank you");
+		System.out.println("hi");
+		
+		
+	
+		
+		
 	}
 
 }
