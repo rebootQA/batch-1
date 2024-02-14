33a2ece32841293ab61ecdc8367d88380e37a6fa
reboot_qa <antsshoes6@gmail.com>
2024-02-13 20:23:56 -0500

    hw0213

---
+++ src/homework/homework_0213.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class homework_0213 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//HW
+//Given a string, return a version without the first and last char, so "Reboot" yields "eboo". The string length will be at least 2.
