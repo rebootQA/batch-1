32cda12e4ad4e4f829e6dd55341a887d65055da2
reboot_qa <antsshoes6@gmail.com>
2024-02-13 20:41:36 -0500

    conflict

--- src/practice_here/hello.java
+++ src/practice_here/hello.java
@@ -3,9 +3,12 @@ package practice_here;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello there");
-		
-		System.out.println("helllllloooo");
+		//System.out.println("Hello there");
+		System.out.println("hey");//
+		//System.out.println("helllllloooo");
+		for (String string : args) {
+			
+		}
 	}
 
 }
