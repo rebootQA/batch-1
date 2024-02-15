f80120690d3cbbb96639777fb3f33b6515d5609b
reboot_qa <antsshoes6@gmail.com>
2024-02-15 18:32:11 -0500

    finished hw

--- src/homework/homework_0214.java
+++ src/homework/homework_0214.java
@@ -3,15 +3,24 @@ package homework;
 public class homework_0214 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String a = "aaa";
+		String b = "b";
+
+		if (a.length() > b.length()) {
+			System.out.println(b + a + b);
+			System.out.println(""+b.length() + a.length()+ b.length());
+		} else {
+			System.out.println(a + b + a);
+			System.out.println(a.length() + "" + b.length() + a.length());
+
+		}
 
 	}
 
 }
 
 /*
- * Given these strings, "reboot" and "homework", 
- * 						"aaa" and "b"
+ * Given these strings, "reboot" and "homework", "aaa" and "b"
  * 
  * return a string of the form short+long+short, with the shorter string on the
  * outside and the longer string on the inside. The strings will not be the same
