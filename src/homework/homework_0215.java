6b13bfbbffa4a96fd163867a442abd269bdff11c
Claudia Ramirez <110501393+laloclau@users.noreply.github.com>
2024-02-16 22:37:38 -0500

    homework015

--- src/homework/homework_0215.java
+++ src/homework/homework_0215.java
@@ -3,6 +3,11 @@ package homework;
 public class homework_0215 {
 
 	public static void main(String[] args) {
+		String word1 = "reboot";
+		// String word2= "Snowing";
+		String firstTwo = word1.substring(0, 2);
+		System.out.println(word1.substring(2) + firstTwo);
+
 		// TODO Auto-generated method stub
 
 	}
@@ -10,12 +15,8 @@ public class homework_0215 {
 }
 
 /*
- * Given these string,
- * "Reboot" and "Snowing"
- * return a "rotated left 2" version where the first 2 chars are
- * moved to the end. 
+ * Given these string, "Reboot" and "Snowing" return a "rotated left 2" version
+ * where the first 2 chars are moved to the end.
  * 
- * expected results :
- * bootRe
- * owingSn
+ * expected results : bootRe owingSn
  */
