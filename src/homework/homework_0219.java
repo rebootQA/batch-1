d8fc3409584247ddbd04838a332ae612fbfa74fc
reboot_qa <antsshoes6@gmail.com>
2024-02-19 21:29:06 -0500

    good luck on this hw

---
+++ src/homework/homework_0219.java
@@ -0,0 +1,29 @@
+package homework;
+
+public class homework_0219 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+ * Given a string 
+ * ("reboot", "CodingBat", "Java")
+ * and an index, 
+ * (0,5,3)
+ * return a string length 2 starting at the given index.
+ 
+ * (reboot,0) ==> re
+ * (CodingBat,5) ==> gB
+ * (Java,3) ==> Ja
+ 
+ // hint : java length = 4
+ // check if index is = 4 
+ // create int variable for index / create string variable words.
+ 
+ * If the index is too big or too small to define a string length 2, use
+ * the first 2 chars. The string length will be at least 2.
+ */
