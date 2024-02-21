3ed80609754f40d82907f49d25ca01f6218cc88b
reboot_qa <antsshoes6@gmail.com>
2024-02-20 20:45:13 -0500

    new hw

---
+++ src/practice_here/homework_0220.java
@@ -0,0 +1,17 @@
+package practice_here;
+
+public class homework_0220 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		/*
+		 * Given a string of odd length, return the string length 3 from its middle, 
+		 * String = "Candy"
+		 * "Candy" yields "and". The string length will be at least 3.
+		 * 
+		 * Hints: Substring
+		 */	}
+
+}
