77c8e6524c064a9da80489cb820d1ee9602f41ac
reboot_qa <antsshoes6@gmail.com>
2024-02-27 18:50:38 -0500

    done

--- src/homework/homework_0226.java
+++ src/homework/homework_0226.java
@@ -3,11 +3,23 @@ package homework;
 public class homework_0226 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		for (int i = 1; i <= 100; i++) {
+
+			if ((i % 3 == 0) && (i % 5 == 0)) {
+				System.out.println(i + " fizzbuzz");
+			} else if (i % 3 == 0) {
+				System.out.println(i + " fizz");
+			} else if (i % 5 == 0) {
+				System.out.println(i + " buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
 
 	}
-
 }
+
 /*
  * write a program that returns "fizz" if the number is a multiplier of 3,
  * return "buzz" if its multiplier of 5, and return "fizzbuzz" if the number is
