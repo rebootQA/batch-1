176823a254f78c161a119895748174d5ebd4f3af
manzire <manzire.anser@gmail.com>
2024-02-27 19:15:28 -0500

    new

---
+++ src/practice_here/jquery.java
@@ -0,0 +1,13 @@
+package practice_here;
+public class jquery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("Webdriver.chrome.driver",
+				"C:\\Users\\dilxa\\OneDrive\\Desktop\\driver\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe\"")
+		
+	
+	}
+
+}
